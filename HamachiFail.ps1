063fc36bb2b60d575b172a16c9e1cb3de0f72a23
Majestic3691 <mtr3691@hotmail.com>
2017-05-17 11:34:54 -0500

    Add files via upload

---
+++ HamachiFail.ps1
@@ -0,0 +1,20 @@
+﻿<# Power shell Script to send email
+    by Michael Ryan
+    Created 17/05/2017
+    #>
+ 
+# Get the state of any services starting Hamachi* from computer Server
+$service = Get-WmiObject win32_service -computername server | select name,state | where { $_.name -like "Hamachi*"} | out-string
+# Specify a sender email address
+$emailFrom = "info@domain.com"
+# Specify a recipient email address
+$emailTo = "op1@domain.com, op2@domain.com, op3@domian.com"
+# Put in a subject line
+$subject = "Hamachi Service Alert"
+# Add the Service state from line 6 to some body text
+$body = $service + "If the service, LogMeIn Hamachi Tunneling Engine, fails to start it could prevent VPN from functioning properly.`nPlease restart it`n`nRegards,`nIT Dept`nSAP Services"
+# Put the DNS name or IP address of your SMTP Server
+$smtpServer = "127.0.0.1"
+$smtp = new-object Net.Mail.SmtpClient($smtpServer)
+# This line pieces together all the info into an email and sends it
+$smtp.Send($emailFrom, $emailTo, $subject, $body)
---
+++ TestEmailServer.ps1
@@ -0,0 +1,18 @@
+﻿<# Power shell Script to test email relay configuration
+    by Michael Ryan
+    Created 17/05/2017
+    #>
+ 
+# Specify a sender email address
+$emailFrom = "info@domain.com"
+# Specify a recipient email address
+$emailTo = "email@outlook.com, 9365551212@vtext.com"
+# Put in a subject line
+$subject = "Test Email Relay"
+# Add the Service state from line 6 to some body text
+$body = "If the email relay is configured correctly, you have received this message"
+# Put the DNS name or IP address of your SMTP Server
+$smtpServer = "127.0.0.1"
+$smtp = new-object Net.Mail.SmtpClient($smtpServer)
+# This line pieces together all the info into an email and sends it
+$smtp.Send($emailFrom, $emailTo, $subject, $body)
